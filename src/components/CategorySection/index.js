99f7c51d20d1c9a291fe9ad9f7e47ce826059803
adprm <aditiyaprmn00@gmail.com>
2020-12-09 13:21:37 +0700

    category component

---
+++ src/components/CategorySection/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Image, Text, View } from 'react-native';
+
+const CategorySection = (props) => {
+    return (
+        <View style={{alignItems: 'center', justifyContent: 'center', flex: 1}}>
+            <View style={{borderColor: 'grey', borderWidth: 1, height: 64, width: 64, marginTop: 10, borderRadius: 32, alignItems: 'center', borderColor: '#dbd9d9', justifyContent: 'center'}}>
+              <Image source={props.img} style={{width: 30, height: 30}} />
+            </View>
+            <Text style={{fontSize: 13, paddingTop: 10, marginBottom: 10}}>{props.title}</Text>
+        </View>
+    )
+}
+
+export default CategorySection;
