8b48f6dcd3b9c0ae3681c2244a70e53ff8fca19c
adprm <aditiyaprmn00@gmail.com>
2020-12-09 17:19:34 +0700

    category section

--- src/components/CategorySection/index.js
+++ src/components/CategorySection/index.js
@@ -7,7 +7,7 @@ const CategorySection = (props) => {
             <View style={{borderColor: 'grey', borderWidth: 1, height: 64, width: 64, marginTop: 10, borderRadius: 32, alignItems: 'center', borderColor: '#dbd9d9', justifyContent: 'center'}}>
               <Image source={props.img} style={{width: 30, height: 30}} />
             </View>
-            <Text style={{fontSize: 13, paddingTop: 10, marginBottom: 10}}>{props.title}</Text>
+            <Text style={{fontSize: 13, paddingTop: 10, marginBottom: 15}}>{props.title}</Text>
         </View>
     )
 }
