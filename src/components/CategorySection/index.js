248a27e843128e616114c390e938f51c78bfba8d
adprm <aditiyaprmn00@gmail.com>
2020-12-11 13:34:01 +0700

    change color border

--- src/components/CategorySection/index.js
+++ src/components/CategorySection/index.js
@@ -4,10 +4,10 @@ import { Image, Text, View } from 'react-native';
 const CategorySection = (props) => {
     return (
         <View style={{alignItems: 'center', justifyContent: 'center', flex: 1}}>
-            <View style={{borderColor: 'grey', borderWidth: 1, height: 64, width: 64, marginTop: 10, borderRadius: 32, alignItems: 'center', borderColor: '#dbd9d9', justifyContent: 'center'}}>
+            <View style={{borderWidth: 1, height: 64, width: 64, marginTop: 10, borderRadius: 32, alignItems: 'center', borderColor: '#0266b8', justifyContent: 'center'}}>
               <Image source={props.img} style={{width: 30, height: 30}} />
             </View>
-            <Text style={{fontSize: 13, paddingTop: 10, marginBottom: 15}}>{props.title}</Text>
+            <Text style={{fontSize: 13, paddingTop: 10, marginBottom: 15, fontWeight: '900'}}>{props.title}</Text>
         </View>
     )
 }
