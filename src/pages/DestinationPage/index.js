b19e58015ce10bc2b955a21aac3de880bdcd2242
adprm <aditiyaprmn00@gmail.com>
2020-12-14 13:15:37 +0700

    destination page

---
+++ src/pages/DestinationPage/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View } from 'react-native';
+import Destination from '../../components/Destination';
+
+const DestinationPage = () => {
+    return (
+        <View style={{backgroundColor: '#e8e8e8', flex: 1}}>
+            <Destination />
+        </View>
+    )
+}
+
+export default DestinationPage;
